13a3174b6526787378b80eac4ee0e9243fbfc5b5
Hontman <leandrogoncalves2004@gmail.com>
2024-05-06 16:14:59 +0100

    U got hit by, u got struck by, chat GPT

--- public/JS/Planetas.js
+++ public/JS/Planetas.js
@@ -1,10 +1,23 @@
 let topic = "";
-export { topic };
-const { processData } = require('./server');
+let facts = ""
+//const index = require('./index.js');
 
-// Use the processData function
-const response = processData();
-console.log(response);
+import OpenAI from 'https://cdn.skypack.dev/openai';
+
+const openai = new OpenAI({
+    apiKey: "sk-proj-FwUk5TLFnSk2Op5OKNqET3BlbkFJVhrr4ndJ7ZV7upZcjqGk", dangerouslyAllowBrowser: true
+});
+async function seeMore(topic) {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "You are a helpful assistant." },
+             { role: "user", content: `Dá-me factos sobre ${topic}` }],
+        model: "gpt-3.5-turbo",
+    });
+    document.getElementById("infoterra").insertAdjacentHTML("afterend", completion.choices[0].message.content);
+}
+
 document.getElementById('terra').addEventListener('click', function() {
     topic = "Planeta Terra";
+    facts = document.getElementById('infoTerra').textContent
+    seeMore(topic)
 });
