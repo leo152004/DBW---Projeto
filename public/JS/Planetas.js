d122b3aa617e83bc1010bf1caf2f7f4187b335b9
Hontman <leandrogoncalves2004@gmail.com>
2024-05-07 16:19:38 +0100

    ITS ALIVE, ITS ALIVEEEEEEEEEEE

--- public/JS/Planetas.js
+++ public/JS/Planetas.js
@@ -7,17 +7,28 @@ import OpenAI from 'https://cdn.skypack.dev/openai';
 const openai = new OpenAI({
     apiKey: "sk-proj-FwUk5TLFnSk2Op5OKNqET3BlbkFJVhrr4ndJ7ZV7upZcjqGk", dangerouslyAllowBrowser: true
 });
-async function seeMore(topic) {
+async function seeMore(topic, facts, addHere) {
     const completion = await openai.chat.completions.create({
         messages: [{ role: "system", content: "You are a helpful assistant." },
-             { role: "user", content: `Dá-me factos sobre ${topic}` }],
+             { role: "user", content: `Dá-me factos sobre ${topic} para além destes: ${facts}` }],
         model: "gpt-3.5-turbo",
     });
-    document.getElementById("infoterra").insertAdjacentHTML("afterend", completion.choices[0].message.content);
+    let factos = document.createElement('p');
+    factos.textContent = completion.choices[0].message.content;
+    factos.style.color = "white";
+    factos.style.fontSize = "24px";
+    factos.style.textAlign = "justify";
+    addHere.appendChild(factos);
 }
 
-document.getElementById('terra').addEventListener('click', function() {
-    topic = "Planeta Terra";
-    facts = document.getElementById('infoTerra').textContent
-    seeMore(topic)
-});
+// Get all the elements with the class 'moreButton'
+var moreButtons = document.getElementsByClassName('moreButton');
+
+// Loop through the buttons and attach the event listener to each one
+for (var i = 0; i < moreButtons.length; i++) {
+    moreButtons[i].addEventListener('click', function(event) {
+    topic = event.target.closest('section').querySelector('h2').textContent;
+    facts = event.target.closest('section').querySelector('p').textContent;
+    let addHere = event.target.closest('section').querySelector('p');
+    seeMore(topic, facts,addHere);
+})}
